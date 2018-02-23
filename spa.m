cf8f45d9817dc8fc5d1ff56a7a35a4c89ada232c
Ding Zhou <dz2336@columbia.edu>
2018-02-23 09:39:33 -0500

    remove old code

--- spa.m
+++
@@ -1,5 +0,0 @@
-m = memmapfile('/Users/zhd/Desktop/Research/Project/IARPA data/first_try/M.mmap','Format',{'double',[5000 61],'V_mat'});
-V_mat = m.Data.V_mat;
-n_comp = size(V_mat,2);
-[rlt,rlt2] = FastSepNMF_dz(V_mat,n_comp,1,0.6);
-save('spa_rlt8.mat','rlt');
